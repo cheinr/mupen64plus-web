f82af1e5bf90b53cb6367c8783e2efd70ab2fb41
Colin <cheinrichs44@gmail.com>
2023-05-06 16:15:34 -0700

    Support for extracting more instructions

--- utils/extract-mips-wat.js
+++ utils/extract-mips-wat.js
@@ -113,6 +113,70 @@ const REGEX_TRANSFORMS = [
     regex: /^i64\.(.+)$/,
     transform: (r) => `    I64_${r[1].toUpperCase()};`
   },
+  {
+    regex: /^block \(result i32\).*$/,
+    transform: (r) => `    I32_BLOCK;`
+  },
+  {
+    regex: /^block \(result i64\).*$/,
+    transform: (r) => `    I64_BLOCK;`
+  },
+  {
+    regex: /^block  ;;.*$/,
+    transform: (r) => `    VOID_BLOCK;`
+  },
+  {
+    regex: /^loop  ;;.*$/,
+    transform: (r) => `    VOID_LOOP;`
+  },
+  {
+    regex: /^if  ;;.*$/,
+    transform: (r) => `    IF;`
+  },
+  {
+    regex: /^if \(result i64\).*$/,
+    transform: (r) => `    IF_I64;`
+  },
+  {
+    regex: /^if \(result i32\).*$/,
+    transform: (r) => `    IF_I32;`
+  },
+  {
+    regex: /^else$/,
+    transform: (r) => `    ELSE;`
+  },
+  {
+    regex: /^br ([0-9]+).*$/,
+    transform: (r) => `    BR(${r[1]});`
+  },
+  {
+    regex: /^br_if ([0-9]+).*$/,
+    transform: (r) => `    BR_IF(${r[1]});`
+  },
+  {
+    regex: /^end.*$/,
+    transform: (r) => `    END;`
+  },
+  {
+    regex: /^select$/,
+    transform: (r) => `    SELECT;`
+  },
+  {
+    regex: /^call \$r4300_write_aligned_dword$/,
+    transform: (r) => `    R4300_WRITE_ALIGNED_DWORD_INDIRECT_CALL;`    
+  },
+  {
+    regex: /^call \$r4300_write_aligned_word$/,
+    transform: (r) => `    R4300_WRITE_ALIGNED_WORD_INDIRECT_CALL;`
+  },
+  {
+    regex: /^call \$r4300_read_aligned_dword$/,
+    transform: (r) => `    R4300_READ_ALIGNED_DWORD_INDIRECT_CALL;`
+  },
+  {
+    regex: /^drop$/,
+    transform: (r) => `    DROP;`
+  }
 ];
 
 // From https://inst.eecs.berkeley.edu/~cs61c/resources/MIPS_help.html
@@ -134,6 +198,8 @@ const R_TYPE_INSTRUCTIONS = [
   "MULTU",
   "DIV",
   "DIVU",
+  "DDIV",
+  "DDIVU",
   "DMULTU",
   "ADD",
   "ADDU",
@@ -421,17 +487,17 @@ let instructions = [/*"RESERVED",*/
                     "SWL",
                     "SWR",
                     "SYNC",
-                    "SYSCALL",
+                    /*"SYSCALL", skipping for now*/
                     "TEQ",
                     "TEQI",
                     "TGE",
                     "TGEI",
                     "TGEIU",
                     "TGEU",
-                    "TLBP",
-                    "TLBR",
-                    "TLBWI",
-                    "TLBWR",
+                    /*"TLBP",
+                       "TLBR",
+                       "TLBWI",
+                       "TLBWR", skipping for now*/
                     "TLT",
                     "TLTI",
                     "TLTIU",
@@ -441,15 +507,11 @@ let instructions = [/*"RESERVED",*/
                     "XOR",
                     "XORI"];
 
-const text = fs.readFileSync('module.wat', { encoding: 'utf8' });
+const text = fs.readFileSync('module5.wat', { encoding: 'utf8' });
 
 let out = ``;
 
-//instructions = [ "ADD",
-//                 "ADDI",
-//                 "ADDIU",
-//                 "ADDU",
-//                 "AND"];
+//instructions = [ "OR" ];
 
 const nonCompileableInstructions = new Set();
 const results = instructions.map((inst) => getInterpFunction(inst));
@@ -476,8 +538,8 @@ console.log("Successfully compiled %o functions", results.length - notSuccessful
 console.log("Unable to compile %o functions", notSuccessfullyCompiled.length);
 
 //console.log(results.filter((result) => result && result.numCompileFailures == 0).map((result) => {
-  //  return Object.assign({}, result, { generated: '' });
-  //}));
+//  return Object.assign({}, result, { generated: '' });
+//}));
 
 function regexTransform(regexp, transformFunc, wasmLine) {
   const execResult = regexp.exec(wasmLine.trim());
@@ -505,10 +567,11 @@ function attemptTransform(wasmLine, instrName) {
     return { value: wasmLine, success: true };
   }
 
-  if (/^call \$r4300_.+$/.exec(wasmLine.trim())) {
+
+  if (/^call \$exception_general$/.exec(wasmLine.trim())) {
     return { value: wasmLine, success: true };
   }
-
+  
   nonCompileableInstructions.add(wasmLine.trim());
   console.log("No transformation exists for instruction: %s!", wasmLine);
   return { value: wasmLine, success: false };
@@ -529,104 +592,140 @@ function performMultiLineReplacements(generatedFunction, name) {
   
   let out = generatedFunction;
   
-  out = out.replace(
-    /I32_CONST\(.+\);\n.+call \$r4300_pc_struct/gmi,
-    "CALL_R4300_PC_STRUCT;");
+  /*  out = out.replace(
+     /I32_CONST\(.+\);\n.+call \$r4300_pc_struct/gmi,
+     "CALL_R4300_PC_STRUCT;");*/
 
   out = out.replace(
-    /I32_CONST\(0\);\n.+generate_block_exit_check\(\);/gmi,
-    "");
+    /I32_CONST\(1747816\);/gmi,
+    "I32_CONST((int) &g_dev.r4300);");
+
+  out = out.replace(
+    /I32_CONST\(1748072\);/gmi,
+    "I32_CONST((int) &(&g_dev.r4300)->hi);");
+
+  out = out.replace(
+    /I32_CONST\(1748080\);/gmi,
+    "I32_CONST((int) &(&g_dev.r4300)->lo);");
+
+  out = out.replace(
+    /I32_CONST\(1748092\);/gmi,
+    "I32_CONST((int) &(&g_dev.r4300)->pc);");
+
+  out = out.replace(
+    /I32_CONST\(1747816\);\n.+call \$exception_general$/gmi,
+    "CALL_EXCEPTION_GENERAL;");
 
 
-  const r4300CallRegexp = /I32_CONST\(.+\);\n.+call \$r4300_(.+)/mi;
-  let r4300CallRegexpExecResult;
-  do {
-    
-    r4300CallRegexpExecResult = r4300CallRegexp.exec(out);
-    
-    if (r4300CallRegexpExecResult) {
-      out = out.replace(r4300CallRegexp,
-                        `R4300_${r4300CallRegexpExecResult[1].toUpperCase()};`);
-      
-    }
-  } while (r4300CallRegexpExecResult);
-    
-
-  
-  if (instructionType) {
-
-    out = out.replace(
-      /CALL_R4300_PC_STRUCT;\n.+I32_LOAD\(0\);\n.+I32_LOAD\(8\);\n.+I32_LOAD\(0\);/gmi,
-      `LOAD_${instructionType}RS32_VALUE;`);
-
-    out = out.replace(
-      /CALL_R4300_PC_STRUCT;\n.+I32_LOAD\(0\);\n.+I32_LOAD\(8\);\n.+I64_LOAD\(0\);/gmi,
-      `LOAD_${instructionType}RS_VALUE;`);
-
-    out = out.replace(
-      /CALL_R4300_PC_STRUCT;\n.+I32_LOAD\(0\);\n.+I32_LOAD\(12\);\n.+I64_LOAD\(0\);/gmi,
-      `LOAD_${instructionType}RT_VALUE;`);
-    
-    out = out.replace(
-      /CALL_R4300_PC_STRUCT;\n.+I32_LOAD\(0\);\n.+I32_LOAD\(12\);\n.+I32_LOAD\(0\);/gmi,
-      `LOAD_${instructionType}RT32_VALUE;`);
-
-    out = out.replace(
-      /CALL_R4300_PC_STRUCT;\n.+I32_LOAD\(0\);\n.+I32_LOAD\(8\);/gmi,
-      `LOAD_${instructionType}RS32_ADDRESS;`);
-
-    out = out.replace(
-      /CALL_R4300_PC_STRUCT;\n.+I32_LOAD\(0\);\n.+I32_LOAD\(12\);/gmi,
-      `LOAD_${instructionType}RT_ADDRESS;`);
-
-    if (instructionType === "R") {
-      
-      out = out.replace(
-        /CALL_R4300_PC_STRUCT;\n.+I32_LOAD\(0\);\n.+I32_LOAD\(16\);/gmi,
-        `LOAD_${instructionType}RD_ADDRESS;`);
-
-      out = out.replace(
-        /CALL_R4300_PC_STRUCT;\n.+I32_LOAD\(0\);\n.+I32_LOAD8_U\(20\);/gmi,
-        `LOAD_RSA32;`);
-
-      out = out.replace(
-        /CALL_R4300_PC_STRUCT;\n.+I32_LOAD\(0\);\n.+I64_LOAD8_U\(20\);/gmi,
-        `LOAD_RSA;`);
-    }
-
-    if (instructionType === "I") {
-      out = out.replace(
-        /CALL_R4300_PC_STRUCT;\n.+I32_LOAD\(0\);\n.+I32_LOAD16_S\(16\);/gmi,
-        `LOAD_IIMMEDIATE_32S;`);
-      out = out.replace(
-        /CALL_R4300_PC_STRUCT;\n.+I32_LOAD\(0\);\n.+I32_LOAD16_U\(16\);/gmi,
-        `LOAD_IIMMEDIATE_32U;`);
-      out = out.replace(
-        /CALL_R4300_PC_STRUCT;\n.+I32_LOAD\(0\);\n.+I64_LOAD16_S\(16\);/gmi,
-        `LOAD_IIMMEDIATE_64S;`);
-      out = out.replace(
-        /CALL_R4300_PC_STRUCT;\n.+I32_LOAD\(0\);\n.+I64_LOAD16_U\(16\);/gmi,
-        `LOAD_IIMMEDIATE_64U;`);
-
-    }
-    //*/
-  }
-
-  const incrementPCRegexp = /CALL_R4300_PC_STRUCT;\n.+LOCAL_TEE\((.+)\);\n.+I32_LOAD\(0\);\n.+I32_CONST\(140\);\n.+I32_ADD;\n.+LOCAL_SET\((.+)\);\n.+LOCAL_GET\(.+\);\n.+LOCAL_GET\(.+\);\n.+I32_STORE\(0\);/mi;
-  incrementPCRegexpExecResult = incrementPCRegexp.exec(out);
-  
-  if (incrementPCRegexpExecResult) {
-    out = out.replace(incrementPCRegexp,
-                      `INCREMENT_PC_BY_ONE(${incrementPCRegexpExecResult[1]}, ${incrementPCRegexpExecResult[2]});`);
-    
-  }
+  /*  out = out.replace(
+     /I32_CONST\(0\);\n.+generate_block_exit_check\(\);/gmi,
+     "");
 
 
+     const r4300CallRegexp = /I32_CONST\(.+\);\n.+call \$r4300_(.+)/mi;
+     let r4300CallRegexpExecResult;
+     do {
+     
+     r4300CallRegexpExecResult = r4300CallRegexp.exec(out);
+     
+     if (r4300CallRegexpExecResult) {
+     out = out.replace(r4300CallRegexp,
+     `R4300_${r4300CallRegexpExecResult[1].toUpperCase()};`);
+     
+     }
+     } while (r4300CallRegexpExecResult);
+   */
+  /*    const r4300CallRegexp = /I32_CONST\(.+\);\n.+call \$r4300_(.+)/mi;
+     let r4300CallRegexpExecResult2;
+     do {
+     
+     r4300CallRegexpExecResult2 = r4300CallRegexp2.exec(out);
+     
+     if (r4300CallRegexpExecResult2) {
+     out = out.replace(r4300CallRegexp2,
+     `R4300_${r4300CallRegexpExecResult[1].toUpperCase()};`);
+     
+     }
+     } while (r4300CallRegexpExecResult2);*/
+
+  /*
+     
+     if (instructionType) {
+
+     out = out.replace(
+     /CALL_R4300_PC_STRUCT;\n.+I32_LOAD\(0\);\n.+I32_LOAD\(8\);\n.+I32_LOAD\(0\);/gmi,
+     `LOAD_${instructionType}RS32_VALUE;`);
+
+     out = out.replace(
+     /CALL_R4300_PC_STRUCT;\n.+I32_LOAD\(0\);\n.+I32_LOAD\(8\);\n.+I64_LOAD\(0\);/gmi,
+     `LOAD_${instructionType}RS_VALUE;`);
+
+     out = out.replace(
+     /CALL_R4300_PC_STRUCT;\n.+I32_LOAD\(0\);\n.+I32_LOAD\(12\);\n.+I64_LOAD\(0\);/gmi,
+     `LOAD_${instructionType}RT_VALUE;`);
+     
+     out = out.replace(
+     /CALL_R4300_PC_STRUCT;\n.+I32_LOAD\(0\);\n.+I32_LOAD\(12\);\n.+I32_LOAD\(0\);/gmi,
+     `LOAD_${instructionType}RT32_VALUE;`);
+
+     out = out.replace(
+     /CALL_R4300_PC_STRUCT;\n.+I32_LOAD\(0\);\n.+I32_LOAD\(8\);/gmi,
+     `LOAD_${instructionType}RS32_ADDRESS;`);
+
+     out = out.replace(
+     /CALL_R4300_PC_STRUCT;\n.+I32_LOAD\(0\);\n.+I32_LOAD\(12\);/gmi,
+     `LOAD_${instructionType}RT_ADDRESS;`);
+
+     if (instructionType === "R") {
+     
+     out = out.replace(
+     /CALL_R4300_PC_STRUCT;\n.+I32_LOAD\(0\);\n.+I32_LOAD\(16\);/gmi,
+     `LOAD_${instructionType}RD_ADDRESS;`);
+
+     out = out.replace(
+     /CALL_R4300_PC_STRUCT;\n.+I32_LOAD\(0\);\n.+I32_LOAD8_U\(20\);/gmi,
+     `LOAD_RSA32;`);
+
+     out = out.replace(
+     /CALL_R4300_PC_STRUCT;\n.+I32_LOAD\(0\);\n.+I64_LOAD8_U\(20\);/gmi,
+     `LOAD_RSA;`);
+     }
+
+     if (instructionType === "I") {
+     out = out.replace(
+     /CALL_R4300_PC_STRUCT;\n.+I32_LOAD\(0\);\n.+I32_LOAD16_S\(16\);/gmi,
+     `LOAD_IIMMEDIATE_32S;`);
+     out = out.replace(
+     /CALL_R4300_PC_STRUCT;\n.+I32_LOAD\(0\);\n.+I32_LOAD16_U\(16\);/gmi,
+     `LOAD_IIMMEDIATE_32U;`);
+     out = out.replace(
+     /CALL_R4300_PC_STRUCT;\n.+I32_LOAD\(0\);\n.+I64_LOAD16_S\(16\);/gmi,
+     `LOAD_IIMMEDIATE_64S;`);
+     out = out.replace(
+     /CALL_R4300_PC_STRUCT;\n.+I32_LOAD\(0\);\n.+I64_LOAD16_U\(16\);/gmi,
+     `LOAD_IIMMEDIATE_64U;`);
+
+     }
+   */
+  //*/
+  /*
+     }
+
+     const incrementPCRegexp = /CALL_R4300_PC_STRUCT;\n.+LOCAL_TEE\((.+)\);\n.+I32_LOAD\(0\);\n.+I32_CONST\(140\);\n.+I32_ADD;\n.+LOCAL_SET\((.+)\);\n.+LOCAL_GET\(.+\);\n.+LOCAL_GET\(.+\);\n.+I32_STORE\(0\);/mi;
+     incrementPCRegexpExecResult = incrementPCRegexp.exec(out);
+     
+     if (incrementPCRegexpExecResult) {
+     out = out.replace(incrementPCRegexp,
+     `INCREMENT_PC_BY_ONE(${incrementPCRegexpExecResult[1]}, ${incrementPCRegexpExecResult[2]});`);
+     
+     }
+
+   */
   return out;
 }
 
 function getInterpFunction(name) {
-  const regexp = new RegExp(`func \\$recomp_wasm_interp_${name}`);
+  const regexp = new RegExp(`func \\$recomp_wasm_interp_${name} `);
   const funcStart = text.search(regexp);
   const funcLength = text.slice(funcStart).search(/\(func/) - 1;
   //out += text.slice(funcStart-1, funcStart+funcLength).replace('type 7', 'type 1');
@@ -641,19 +740,23 @@ function getInterpFunction(name) {
 
   functionBodyLines[functionBodyLines.length - 1] = functionBodyLines[functionBodyLines.length - 1].replace(")", "");
   console.log(functionBodyLines);
-  
-  const localDeclarations = functionBodyLines[1].trim().replace("(", "").replace(")", "").replace("local ", "").split(" ");
 
   let generated = `static void wasm_gen_${name}(struct precomp_instr* inst) {\n\n`;
-  //generated += `    printf("generating: ${name}; wasm_code_length=%u", wasm_code_length);\n`;
-  localDeclarations.forEach((localType, index) => {
-    generated += `    uint32_t local${index} = claim_${localType}_local();\n`;
-  });
+
+  let startLine = 1;
+  if (functionBodyLines[1].includes("(local")) {
+    startLine = 2;
+    const localDeclarations = functionBodyLines[1].trim().replace("(", "").replace(")", "").replace("local ", "").split(" ");
+    //generated += `    printf("generating: ${name}; wasm_code_length=%u", wasm_code_length);\n`;
+    localDeclarations.forEach((localType, index) => {
+      generated += `    uint32_t local${index} = claim_${localType}_local();\n`;
+    });
+  }
 
   generated += `\n`;
 
   let numFailures = 0;
-  for (let i = 2; i < functionBodyLines.length; i++) {
+  for (let i = startLine; i < functionBodyLines.length; i++) {
     const transformResult = attemptTransform(functionBodyLines[i], name);
     if (!transformResult.success) {
       numFailures++;
